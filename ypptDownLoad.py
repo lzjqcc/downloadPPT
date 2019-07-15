0af9d961d559407d9710df1f5eb16daa783d722e
lizhijian <lizhijian@mytian.com>
2019-07-15 17:47:22 +0800

    修复 yppt下载

--- ypptDownLoad.py
+++ ypptDownLoad.py
@@ -11,9 +11,10 @@ headers = {
     "Upgrade-Insecure-Requests":1,
     "ACCPT":'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
 }
-rootPath = '/home/li/download/';
+rootPath = "D:\\ppt\\";
+fileSplit ="\\";
+
 def getRequestContent(url,proxy=False):
-    http = None;
     if proxy:
         http = Proxy().getProxyHttp(headers);
     else:
@@ -41,9 +42,17 @@ def download(classifyName,classifyHref):
     if not os.path.exists(rootPath + classifyName):
         os.mkdir(rootPath + classifyName);
     downloadAll(classifyHref,classifyHref, classifyName, 0)
+def getMaxPageIndex(pageLinkAs):
+    pageLinkAsReverse = pageLinkAs[::-1];
+    for pageLinkA in pageLinkAsReverse:
+        if pageLinkA.string.isdigit():
+            return int(pageLinkA.string);
+
+
 def downloadAll(href,preFixHref, classifyName, currentPage):
     classifyPage = getPage(href);
     pageLinkAs = classifyPage.select('.page-navi')[0].select('a');
+    maxPageIndex = getMaxPageIndex(pageLinkAs);
     downloadOnePagePPT(href, classifyName)
     for pageHrefIndex in range(len(pageLinkAs)):
         #当前页的链接
@@ -51,19 +60,19 @@ def downloadAll(href,preFixHref, classifyName, currentPage):
         #当前页的数字  1,2,3,4,5
         pageIndexString = pageLinkAs[pageHrefIndex].string;
         #判断是否是数字
-        if pageIndexString.isdigit:
+        if pageIndexString.isdigit():
             pageIndex = int(pageIndexString)
         else:
             continue
         if pageIndex <= currentPage:
             continue
 
-        if pageHrefIndex == len(pageLinkAs) -1:
-            download(preFixHref +'/' +pageHref,preFixHref, classifyName, pageIndex);
+        if pageIndex == maxPageIndex:
+            downloadAll(preFixHref + '/' + pageHref, preFixHref, classifyName, pageIndex);
         else:
-            print(pageHref)
             downloadOnePagePPT(preFixHref + '/' + pageHref, classifyName);
-            print("下载第"+pageIndexString+"页")
+            print(classifyName + "下载第"+pageIndexString+"页" + preFixHref + '/' + pageHref)
+
 def downloadOnePagePPT(href, classifyName):
     soup = getPage(href);
     print(soup)
@@ -71,7 +80,7 @@ def downloadOnePagePPT(href, classifyName):
     for a in posts.select("a"):
         if a.string is not None:
             name = a.string;
-            if os.path.exists(rootPath + classifyName + '/' + name +'.rar'):
+            if os.path.exists(rootPath + classifyName + fileSplit + name +'.rar'):
                 continue
             pptPageHref = a.attrs["href"];
             pptPage = getPage(pptPageHref);
@@ -82,16 +91,16 @@ def downloadOnePagePPT(href, classifyName):
             print(down.attrs['href'])
             ppt = getRequest(down.attrs['href']);
             print("下载:"+name)
-            with open(rootPath + classifyName + "/" + name + ".rar", "wb") as f:
+            with open(rootPath + classifyName + fileSplit + name + ".rar", "wb") as f:
                 f.write(ppt.data)
             f.close()
-# mobanPage = getPage('/moban');
-# menu = mobanPage.select('.menu ul')[0];
-# for a in menu.select('a'):
-#     if a.string != '动态模板':
-#         classifyName = a.string;
-#         classifyHref = a.attrs['href'];
-#         download(classifyName, classifyHref);
-download('商务模板','/moban/shangwu/');
+mobanPage = getPage('/moban');
+menu = mobanPage.select('.menu ul')[0];
+for a in menu.select('a'):
+    if a.string != '动态模板':
+        classifyName = a.string;
+        classifyHref = a.attrs['href'];
+        threading.Thread(target=download, args=(classifyName, classifyHref)).start()
+
 
 
